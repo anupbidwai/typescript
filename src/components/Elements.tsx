435090eeb68c29c3f9ba7a817b459a44022ccf84
Anup Bidwai <bidwaiav@gmail.com>
2023-05-01 14:52:38 +0530

    practice on typescript + reactjs

---
+++ src/components/Elements.tsx
@@ -0,0 +1,30 @@
+type ButtonProps = {
+    id?: string,
+    type: "button" | "submit"
+    label: string,
+    onClick?: (...args: any[]) => any
+}
+
+type InputProps = {
+    id?: string,
+    type: string,
+    placeholder: string,
+    name?: string,
+    value: string,
+    onChange: (...args: any[]) => any
+}
+
+const Button = (props: ButtonProps) => {
+    const { label, type = "button", ...rest } = props;
+    return <button type={type} {...rest}>{label}</button>;
+};
+
+
+const Input = (props: InputProps) => <input {...props} />;
+
+
+export {
+    Button,
+    Input
+};
+
